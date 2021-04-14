91da794101cb31a4fc91b87b6dd0aec6bf96d966
emjose <emjose@gmail.com>
2021-04-13 23:14:18 -0400

    update coin index

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -1,16 +1,49 @@
-const Coins = ({name, price, symbol, marketcap, volume, image, priceChange, id}) => {
-    return (
-        <div className="coin_container">
-            <div className="coin_row">
-                <div className="coin">
-                    <img src={image} alt={name}
-                    className="coin_img"/>
-                    <h1 className="coin_h1">{name}</h1>
-                    <p className="coin_cymbol">{symbol}</p>
-                </div>
-            </div>
-        </div>
-    )
-}
+import styles from './Coins.module.css';
+import Link from 'next/link';
 
-export default Coins
+const Coins = ({
+  name,
+  price,
+  symbol,
+  marketcap,
+  volume,
+  image,
+  priceChange,
+  id
+}) => {
+  return (
+    <Link href='/coin/[id]' as={`/coin/${id}`}>
+      <a>
+        <div className={styles.coin_container}>
+          <div className={styles.coin_row}>
+            <div className={styles.coin}>
+              <img src={image} alt={name} className={styles.coin_img} />
+              <h1 className={styles.coin_h1}>{name}</h1>
+              <p className={styles.coin_symbol}>{symbol}</p>
+            </div>
+            <div className={styles.coin_data}>
+              <p className={styles.coin_price}>${price}</p>
+              <p className={styles.coin_volume}>${volume.toLocaleString()}</p>
+
+              {priceChange < 0 ? (
+                <p className={(styles.coin_percent, styles.red)}>
+                  {priceChange.toFixed(2)}%
+                </p>
+              ) : (
+                <p className={(styles.coin_percent, styles.green)}>
+                  {priceChange.toFixed(2)}%
+                </p>
+              )}
+
+              <p className={styles.coin_marketcap}>
+                Mkt Cap: ${marketcap.toLocaleString()}
+              </p>
+            </div>
+          </div>
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+export default Coins;
