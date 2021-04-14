0cc1eecf52bd648cd90782dab60455f8704adee3
emjose <emjose@gmail.com>
2021-04-14 00:01:44 -0400

    update api

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home({ filteredCoins }) {
 }
 
 export const getServerSideProps = async () => {
-  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
 
   const filteredCoins = await res.json()
 
