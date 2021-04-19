d5cd63653108eb189e9ff2c360e3f5c280c49cb9
emjose <emjose@gmail.com>
2021-04-19 12:24:40 -0400

    install tilty

--- pages/coin/[id].js
+++ pages/coin/[id].js
@@ -1,7 +1,11 @@
 import Layout from '../../components/Layout';
 import styles from './Coin.module.css';
 
+
 const Coin = ({ coin }) => {
+
+
+
   return (
     <Layout>
       <div className={styles.coin_page}>
