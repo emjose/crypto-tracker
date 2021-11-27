6b49391b3c92a8324b2369216391d4827c3511d2
emmanueljose <emjose@gmail.com>
2021-11-26 19:50:05 -0500

    Github ribbon

--- components/Layout.js
+++ components/Layout.js
@@ -1,27 +1,27 @@
-import Head from 'next/head';
-import Link from 'next/link';
+import Head from "next/head";
+import Link from "next/link";
+import { Github } from "./github";
 
-const Layout = ({ children, title = 'Crypto Tracker' }) => {
-  return (
-    
-    <div className='layout'>
-      
-      <Head>
-        <title>{title}</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      
-      <header className='header'>
-        <Link href='/' passHref>
-          <a><img className='logo' src="/ct-logo.png" alt="Crypto Tracker"/> </a>
-        </Link>
-      </header>
+const Layout = ({ children, title = "Crypto Tracker" }) => {
+	return (
+		<div className="layout">
+			<Head>
+				<title>{title}</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main>{children}</main>
-      
-    </div>
+			<header className="header">
+				<Link href="/" passHref>
+					<a>
+						<img className="logo" src="/ct-logo.png" alt="Crypto Tracker" />{" "}
+					</a>
+				</Link>
+				<Github />
+			</header>
 
-  );
+			<main>{children}</main>
+		</div>
+	);
 };
 
 export default Layout;
